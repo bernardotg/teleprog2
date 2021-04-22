afabea572a2bbc5e47676a562e9b4e503d38a436
bernardo <bernardotorresguaido@gmail.com>
2021-04-22 15:53:04 -0400

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Progr2!");
+        }
+    }
+}
